4d06af7d6c8c90a7da4fa306b86776254a88ed55
Prince Ajuobi <princekelsey299@gmail.com>
2020-04-15 00:36:36 +0300

    added header component, sass and styles structure

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,17 @@
 import React from "react";
-import "./header.styles.css";
 
 const Header = () => {
   return (
     <header className="header">
-      <div className="logo-container">
-        <img src="img/logo-white.png" alt="logo" className="logo" />
+      <div className="header__logo-container">
+        <img src="img/logo-white.png" alt="logo" className="header__logo" />
       </div>
-      <div className="heading-container">
+      <div className="header__heading-container">
         <h1 className="heading">
-          <span className="heading-main">Outdoors</span>
-          <span className="heading-sub">is where life happens</span>
+          <span className="heading--main">Outdoors</span>
+          <span className="heading--sub">is where life happens</span>
         </h1>
-        <a href="#" className="btn btn-white">
+        <a href="#" className="btn btn--white btn--animated">
           Discover our tours
         </a>
       </div>
